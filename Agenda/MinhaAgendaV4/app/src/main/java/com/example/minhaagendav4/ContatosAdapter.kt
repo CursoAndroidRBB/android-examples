7901b588edf96d56971bc46058e8de55d61eb36e
Rodrigo Bernardino <rbbernardino@gmail.com>
2021-10-11 10:21:27 -0300

    adiciona exemplo de recyclerview com clique

---
+++ Agenda/MinhaAgendaV4/app/src/main/java/com/example/minhaagendav4/ContatosAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.minhaagendav4
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.minhaagendav4.databinding.ItemContatoBinding
+
+class ContatosAdapter(
+    val listaContatos: MutableList<Contato>,
+    val onItemClick: (Int) -> Unit
+    ) : RecyclerView.Adapter<ContatosAdapter.ViewHolder>() {
+
+    inner class ViewHolder(val binding: ItemContatoBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemContatoBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            ))
+    }
+
+    override fun getItemCount(): Int {
+        return listaContatos.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.txtNome.text = listaContatos[position].nome
+        holder.binding.txtTelefone.text = listaContatos[position].telefone
+        holder.binding.root.setOnClickListener {
+            onItemClick(position)
+        }
+    }
+
+    fun swapData(novaListaContatos: List<Contato>){
+        listaContatos.clear()
+        listaContatos.addAll(novaListaContatos)
+        notifyDataSetChanged()
+    }
+}
